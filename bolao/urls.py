72b95e570e15c48c8b865986b708733e1d331972
André Pennini <andrepennini@usp.br>
2025-05-30 17:28:45 -0300

    arrumado algumas coisas dos forms

--- bolao/urls.py
+++ bolao/urls.py
@@ -9,21 +9,21 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     
     # Password Reset URLs
-    path('password_reset/', auth_views.PasswordResetView.as_view(
-        template_name='bolao/auth/password_reset.html',
-        email_template_name='bolao/auth/password_reset_email.html',
-        subject_template_name='bolao/auth/password_reset_subject.txt'
-    ), name='password_reset'),
+    # path('password_reset/', auth_views.PasswordResetView.as_view(
+    #     template_name='bolao/auth/password_reset.html',
+    #     email_template_name='bolao/auth/password_reset_email.html',
+    #     subject_template_name='bolao/auth/password_reset_subject.txt'
+    # ), name='password_reset'),
     
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(
-        template_name='bolao/auth/password_reset_done.html'
-    ), name='password_reset_done'),
+    # path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(
+    #     template_name='bolao/auth/password_reset_done.html'
+    # ), name='password_reset_done'),
     
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
-        template_name='bolao/auth/password_reset_confirm.html'
-    ), name='password_reset_confirm'),
+    # path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+    #     template_name='bolao/auth/password_reset_confirm.html'
+    # ), name='password_reset_confirm'),
     
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
-        template_name='bolao/auth/password_reset_complete.html'
-    ), name='password_reset_complete'),
+    # path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
+    #     template_name='bolao/auth/password_reset_complete.html'
+    # ), name='password_reset_complete'),
 ]
--- bolao/views.py
+++ bolao/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate
 
 # Create your views here.
 def index(request):
@@ -31,18 +34,18 @@ def login_view(request):
     Handle user login.
     """
     if request.method == 'POST':
-        form = UserCreationForm(request, data=request.POST)
-        if form.is_valid():
-            user = form.get_user()
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
             login(request, user)
             messages.success(request, 'Login realizado com sucesso!')
             return redirect('index')
         else:
             messages.error(request, 'Usuário ou senha inválidos.')
-    else:
-        form = UserCreationForm()
     
-    return render(request, 'bolao/auth/login.html', {'form': form})
+    return render(request, 'bolao/auth/login.html', {'form': UserCreationForm()})
 
 def logout_view(request):
     """
